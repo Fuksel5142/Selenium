c9eaebdd1a5130f67d439086f72dfeca37c9fe2e
fatihhhh <fatihisiteknik42@gmail.com>
2022-09-24 22:14:37 +0300

    v04

---
+++ src/test/java/Day13/C03_WebTable.java
@@ -0,0 +1,35 @@
+package Day13;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C03_WebTable extends TestBase {
+    @Test
+    public void test01() {
+        // onceki class'daki adrese gidip
+        // girisYap method'unu kullanarak sayafaya giris yapin
+        // input olarak verilen satir sayisi ve sutun sayisi'na sahip
+        // cell'deki text'i yazdirin
+        login();
+        int satir=3;
+        int sutun=3;
+        WebElement aranancell=driver.findElement(By.xpath("//tbody//tr["+satir+"]//td["+sutun+"]"));//çmce tr olur sonra td
+        //tr satır td sütün satır komple alınır sütün tek tek doldurulur
+        System.out.println(aranancell.getText());
+    }
+
+    public void login() {
+        driver.get("https://www.hotelmycamp.com");
+        driver.findElement(By.xpath("//*[text()='Log in']")).click();
+      Actions action = new Actions(driver);
+        WebElement username = driver.findElement(By.id("UserName"));
+        action.click(username).sendKeys("manager")
+                .sendKeys(Keys.TAB).sendKeys("Manager1!").
+                sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+
+    }
+}
