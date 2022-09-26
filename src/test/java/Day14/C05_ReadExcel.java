b0dbc9751a7bc27813411299508f1abf834f8bd5
fatihhhh <fatihisiteknik42@gmail.com>
2022-09-26 18:35:42 +0300

    v04

---
+++ src/test/java/Day14/C05_ReadExcel.java
@@ -0,0 +1,34 @@
+package Day14;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C05_ReadExcel {
+    @Test
+    public void readExcelTest1() throws IOException {
+        //-Dosya yolunu bir String degiskene atayalim
+        String dosyaYolu="src/resources/ulkeler.xlsx";
+        //-FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
+        FileInputStream fis = new FileInputStream(dosyaYolu);
+        //-Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
+        Workbook workbook = WorkbookFactory.create(fis);
+        //-WorkbookFactory.create(fileInputStream)
+        //-sayfa 2'ye gidip satir sayisinin 6, kullanilan satir sayisinin ise 3 oldugunu test edin
+        int sonSatirIndex=workbook
+                .getSheet("Sayfa2")
+                .getLastRowNum();
+        int expectedSatirsayisi=6;
+        System.out.println(sonSatirIndex);
+        Assert.assertNotEquals(expectedSatirsayisi,sonSatirIndex);
+        int kullanilanSatirsayisi=workbook.getSheet("Sayfa2").getPhysicalNumberOfRows();
+        int expectedactualSatirsayisi=3;
+        Assert.assertNotEquals(kullanilanSatirsayisi,expectedactualSatirsayisi);
+
+    }
+}
